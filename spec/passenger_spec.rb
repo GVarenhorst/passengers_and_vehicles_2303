d994b943279c24a3dd18e5b8546b31f1f42a4aa0
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-05 10:38:38 -0500

    Create and pass 'exists' and 'age' tests

---
+++ spec/passenger_spec.rb
@@ -0,0 +1,21 @@
+
+require './lib/passenger'
+
+RSpec.describe Passenger do
+  before(:each) do
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})    
+  end
+
+  it 'exists' do
+    expect(@charlie).to be_an_instance_of(Passenger)
+    expect(@taylor).to be_an_instance_of(Passenger)
+  end
+
+  it 'has an age' do
+    expect(@charlie.age).to eq(18)  
+    expect(@taylor.age).to eq(12)
+  end
+
+  it ''
+end
