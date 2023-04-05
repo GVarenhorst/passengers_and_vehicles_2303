d9dd25868e30dc37c63b9ec4c04d577a68047aae
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-05 12:02:24 -0500

    Attempt to add can_drive method

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,14 +1,22 @@
 class Passenger
   attr_reader :name,
-              :age
+              :age,
+              :drive
 
   def initialize(attributes)
     @name = attributes["name"]
     @age = attributes["age"]
+    @driver = []
   end
 
   def adult?
     @age >= 18 == true
   end
 
+  def can_drive
+    if adult? && @name.drive
+      driver
+    end
+    @driver << driver
+  end
 end
