44925c4c7dee6d18413346423a0abc235e4ceab5
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-05 10:47:55 -0500

    Test and pass adult? method for passenger.rb

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -8,14 +8,20 @@ RSpec.describe Passenger do
   end
 
   it 'exists' do
+
     expect(@charlie).to be_an_instance_of(Passenger)
     expect(@taylor).to be_an_instance_of(Passenger)
   end
 
   it 'has an age' do
+
     expect(@charlie.age).to eq(18)  
     expect(@taylor.age).to eq(12)
   end
 
-  it ''
+  it 'can verify who is an adult' do
+
+    expect(@charlie.adult?).to eq(true)
+    expect(@taylor.adult?).to eq(false)
+  end
 end
